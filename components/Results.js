23ff308ace9bd9dc33844931c961d017a70e5dc1
KeshavVats <82632075+Keshavvats9470@users.noreply.github.com>
2023-02-15 16:49:40 +0530

    Update Results.js

--- components/Results.js
+++ components/Results.js
@@ -1,7 +1,7 @@
 import Thumbnail from "./Thumbnail";
 import FlipMove from "react-flip-move";
 
-function Results({ results }) {
+function Results({ results })  {
   return (
   <FlipMove className="px-4 my-11 sm:grid md:grid-cols-2 xl:grid-cols-4 3xl:flex flex-wrap justify-center "> 
     {results.map( (result) => (
